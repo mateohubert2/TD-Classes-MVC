bb4ad7609e94ae9364eb9769525405f7527c8384
mateohubert2 <mathub2003@gmail.com>
2024-05-11 17:52:46 +0200

    Suppression des elements inutiles

--- js/application/application.js
+++ js/application/application.js
@@ -2,16 +2,8 @@ import { View } from "./../views/view.js"
 import { Controller } from "./../controllers/controller.js"
 import { Counter } from "./../models/counter.js"
 
-const counter = new Counter();
-function afficher(controller){
-    //console.log(counter.getValue());
-    console.log(controller.getCounterValue());
+function creation(){
+    const controller = new Controller();
+    const view = new View(controller);
 }
-//function creation(){
-//    const controller = new Controller();
-//    const view = new View(controller);
-//}
-//window.addEventListener("load", creation);
-let controller = new Controller();
-let view = new View(controller);
-window.addEventListener("load", afficher(controller));
+window.addEventListener("load", creation);
